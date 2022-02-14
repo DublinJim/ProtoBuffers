db64551cacf4ef491f730194d624a3afb3e430a3
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-14 10:28:31 -0500

    initial commit javascript json

---
+++ index.js
@@ -0,0 +1,8 @@
+const employees = []
+
+employees.push({ Name: 'James', ID: 34 })
+employees.push({ Name: 'Bill', ID: 36 })
+employees.push({ Name: 'Janet', ID: 24 })
+
+
+console.log(JSON.stringify(employees))
